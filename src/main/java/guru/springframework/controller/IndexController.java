5420fc96178cfa3c63981a2a263b41292b62e370
tcn741 <tianchenlko@126.com>
2019-04-25 18:57:10 +0800

    init local repository

---
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -0,0 +1,12 @@
+package guru.springframework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+   @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
